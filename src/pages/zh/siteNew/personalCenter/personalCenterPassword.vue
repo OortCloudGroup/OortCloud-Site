39f82d6f62e953054339b0e2a6ad91a9f9de76d1
chenqinglin <cql@qq.com>
2025-01-21 18:05:51 +0800

    feat:  个人中心 页面整理

---
+++ src/pages/zh/siteNew/personalCenter/personalCenterPassword.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="personalPage_sub">
+    <div class="menuCont_t">
+      密码安全
+    </div>
+    <div>更改或查看密码</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.personalPage_sub {
+  height: 100%;
+  width: 100%;
+  background-color: #fff;
+
+  .menuCont_t {
+    width: 100px;
+    height: 28px;
+    font-weight: 700;
+    font-size: 20px;
+    color: #111827;
+    line-height: 28px;
+    text-align: left;
+    font-style: normal;
+  }
+}
+
+</style>
