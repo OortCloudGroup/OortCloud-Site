9199c5ed42de81adfce3259a038e7dab0e2274a6
chenqinglin <cql@qq.com>
2025-02-26 17:34:32 +0800

    feat: 立即购买

--- src/pages/zh/siteNew/industy/buy.vue
+++ src/pages/zh/siteNew/industy/buy.vue
@@ -7,28 +7,50 @@
       <el-input v-model="value" class="bottom_pro_address" placeholder="订阅邮箱，请输入您的邮箱" />
     </div>
     <div class="flexRowAC buyAppsOut">
-      <div v-for="(item,i) in appsList" :key="i">
-        <div class="buyAppsOut_t flexRowAC">
-          <img class="buyAppsOut_t_img" src="@/assets/software/price_c1.png" alt="" />
-          {{ item.t }}
-        </div>
-        <div class="proTitleBox priceIconBox w1380 flexRowAC">
-          <div v-for="(itd,index) in item.child" :key="index" class="priceIconIt flexRowAC">
-            <div class="flexRowAC">
+      <div v-for="(item,i) in appsList" :key="i" class="buyAppsList">
+        <el-checkbox
+          v-model="checkAll"
+          class="elCheckbox_all"
+          :indeterminate="isIndeterminate"
+          @change="handleCheckAllChange"
+        >
+          <div class="buyAppsOut_t flexRowAC">
+            <img class="buyAppsOut_t_img" src="@/assets/software/price_c1.png" alt="" />
+            {{ item.t }}
+          </div>
+        </el-checkbox>
+        <el-checkbox-group
+          v-model="checkedCities"
+          class="priceIconBox"
+          @change="handleCheckedCitiesChange"
+        >
+          <el-checkbox
+            v-for="(itd,index) in item.child"
+            :key="index"
+            :label="itd.t"
+            :value="itd"
+          >
+            <div class="flexRowAC priceIconIt">
               <img class="priceImg" src="@/assets/software/price_c1.png" alt="" />
               <div class="priceTi">
                 {{ itd.t }}
               </div>
             </div>
-          </div>
-        </div>
+          </el-checkbox>
+        </el-checkbox-group>
       </div>
       <div class="buyApps">
         <div class="buyApps_t">
-          已选择 5 Apps
+          已选择 {{ checkedCities.length }} Apps
         </div>
-        <div>
-          购买
+        <div v-for="(item,i) in checkedCities" :key="i">
+          <div class="buyAppsOut_t flexRowAC t">
+            <img class="buyAppsOut_t_img" src="@/assets/software/price_c1.png" alt="" />
+            {{ item.t }}
+          </div>
+        </div>
+        <div :class="{opa: !checkedCities.length}" class="buyBtn flexRowAC" @click="buyClick()">
+          立即购买
         </div>
       </div>
     </div>
@@ -45,67 +67,97 @@ definePageMeta({
 
 let value = ref('')
 let appsList = ref([])
+const checkAll = ref(false)
+const isIndeterminate = ref(true)
+const checkedCities = ref([])
 appsList.value = [
   {
     t: '个人效率工具',
     img: price_c1,
     child: [
       { t: '云盘', img: '' },
-      { t: '云盘', img: '' },
-      { t: '云盘', img: '' },
-      { t: '云盘', img: '' },
-      { t: '云盘', img: '' }
+      { t: '云相册', img: '' },
+      { t: '云文档', img: '' },
+      { t: '云清单', img: '' },
+      { t: '云备忘录', img: '' },
+      { t: '电子邮件', img: '' }
     ]
   }
 ]
+
+const handleCheckAllChange = (val) => {
+  checkedCities.value = val ? appsList.value[0]?.child : []
+  isIndeterminate.value = false
+}
+const handleCheckedCitiesChange = (value) => {
+  const checkedCount = value.length
+  checkAll.value = checkedCount === appsList.value[0]?.child.length
+  isIndeterminate.value = checkedCount > 0 && checkedCount < appsList.value[0]?.child.length
+}
+
+// 立即购买<NuxtLink to="http://oort.oortcloudsmart.com:23410/bus/apaas-web/console_manage/index.html"
+const buyClick = () => {
+  if (!checkedCities.value.length) return false
+  window.open('http://oort.oortcloudsmart.com:23410/bus/apaas-web/console_manage/index.html', '_blank')
+}
 </script>
 
 <style scoped lang="scss">
-.buyAppsOut {
-  padding: 0 160px 100px;
-  align-items: flex-start;
-
-  &_t {
-    padding-bottom: 20px;
-    font-weight: 700;
-    font-size: 20px;
-    color: #3D3D3D;
-    line-height: 24px;
-    gap: 8px;
-  }
-
-  &_t_img{
-    width: 24px;
-    height: 24px;
-  }
-}
-
-.buyApps {
-  margin-top: 44px;
-  background-color: #fff;
-  width: 200px;
-  padding: 28px;
-  flex-shrink: 0;
-
-  .buyApps_t {
-    font-weight: 700;
-    font-size: 28px;
-    color: #3D3D3D;
-    line-height: 42px;
-  }
-}
-
 .buyPage {
   position: relative;
-}
-
-.buyPage {
   background-color: #f7f7f7;
 }
 
 .w1380 {
   width: 1380px;
   margin: 0 auto;
+}
+
+.buyAppsOut {
+  padding: 0 160px 100px;
+  align-items: flex-start;
+
+  .buyAppsList {
+    flex: 1;
+  }
+
+  .elCheckbox_all{
+    padding-bottom: 20px;
+  }
+}
+
+.buyAppsOut_t {
+  font-weight: 700;
+  font-size: 20px;
+  color: #3D3D3D;
+  line-height: 24px;
+  gap: 8px;
+
+  .buyAppsOut_t_img{
+    width: 24px;
+    height: 24px;
+  }
+
+  &.t {
+    padding-bottom: 20px;
+  }
+}
+
+.buyApps {
+  margin-top: 44px;
+  background-color: #fff;
+  width: 420px;
+  padding: 28px;
+  flex-shrink: 0;
+  min-height: 200px;
+
+  .buyApps_t {
+    padding-bottom: 20px;
+    font-weight: 700;
+    font-size: 28px;
+    color: #3D3D3D;
+    line-height: 42px;
+  }
 }
 
 .platTop {
@@ -132,34 +184,44 @@ appsList.value = [
   }
 }
 
-.proTitleBox {
-  color: #3D3D3D;
-  font-weight: bold;
-  font-size: 58px;
+// 立即购买
+.buyBtn{
+  cursor: pointer;
+  margin-top: 20px;
+  justify-content: center;
+  color: #FFFFFF;
+  background: #2278FF;
+  height: 72px;
+  border-radius: 8px 8px 8px 8px;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+
+  &.opa {
+    opacity: .5;
+    cursor: not-allowed;
+  }
 }
 
-.priceIconBox {
-  flex-wrap: wrap;
-  gap: 20px;
-
-  .priceIconIt {
+// 个人效率工具
+:deep(.priceIconBox ) {
+  .el-checkbox{
+    width: calc(25% - 60px);
     background-color: #fff;
-    width: 280px;
-    padding: 12px 20px;
-    gap: 20px;
-  }
-
-  .priceImg {
-    width: 56px;
+    padding: 10px ;
+    margin-bottom: 20px;
     height: 56px;
   }
+  .priceIconIt {
+    .priceImg {
+      width: 56px;
+      height: 56px;
+    }
+  }
 
-  .priceTi {
-    color: #3D3D3D;
-    font-weight: normal;
-    font-size: 20px;
-    text-align: center;
+  .el-checkbox__input{
+    position: absolute;
+    right: 20px;
   }
 }
-
 </style>
